fd190d52adcb485d7a0d9f089536476312dbf17f
Jonnathan Matute <jmatutec2@gmail.com>
2019-04-25 19:54:45 -0500

    index
    Creacion de iniciar sesion

--- public/controladores/login.php
+++ public/controladores/login.php
@@ -1,19 +1,19 @@
-<?php
-session_start();
-include '../../config/conexionBD.php';
-$usuario = isset($_POST["correo"]) ? trim($_POST["correo"]) : null;
-$contrasena = isset($_POST["contrasena"]) ? trim($_POST["contrasena"]) : null;
+<?php 
+session_start(); 
+include '../../config/conexionBD.php'; 
+$usuario = isset($_POST["correo"]) ? trim($_POST["correo"]) : null; 
+$contraseña = isset($_POST["contraseña"]) ? trim($_POST["contraseña"]) : null; 
 
-$sql = "SELECT * FROM usuario WHERE usu_correo = '$usuario' and usu_password = MD5('$contrasena')";
+$sql = "SELECT * FROM usuario WHERE usu_correo = '$usuario' and usu_password = MD5('$contraseña')";
 
-$result = $conn->query($sql);
-if ($result->num_rows > 0) {
-    $_SESSION['isLogged'] = TRUE;
+$result = $conn->query($sql); 
+if ($result->num_rows > 0) { 
+    $_SESSION['isLogged'] = TRUE; 
     header("Location: ../../admin/vista/usuario/index.php");
-} else {
-    header("Location: ../vista/login.html");
-}
-$conn->close();
-
+    } else {
+        header("Location: ../vista/login.html");
+        } 
+        
+        $conn->close(); 
 
 ?>
